1b98be88a6015cae34aab4cbfb4b46fa6ce4ff96
tesao <shimeji9@gmail.com>
2020-04-02 23:45:09 +0900

    HelloWorldの削除, Navbarの追加, eslint ruleの追加

---
+++ src/components/HeaderBar.vue
@@ -0,0 +1,33 @@
+<template lang="html">
+  <div class="centerx">
+    <vs-navbar class="navBarColor" color="dark" text-color="#FFFFFF">
+      <div slot="title">
+        <vs-navbar-title>
+          Portfolio
+        </vs-navbar-title>
+      </div>
+
+      <vs-navbar-item index="0">
+        <a href="#">Home</a>
+      </vs-navbar-item>
+      <vs-navbar-item index="1">
+        <a href="#">About Me</a>
+      </vs-navbar-item>
+      <vs-navbar-item index="2">
+        <a href="#">Works</a>
+      </vs-navbar-item>
+    </vs-navbar>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HeaderBar"
+}
+</script>
+
+<style lang="css" scoped>
+.navBarColor {
+  color: rgb(255, 255, 255);
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Vuesax from 'vuesax'
-import App from './App.vue'
+import App from '@/App.vue'
+import 'vuesax/dist/vuesax.css'
 
 Vue.config.productionTip = false
 
